6de492df599d989e970daf9ab71979ec566414c9
Stanisław Zakrzewski <stanislaw.zakrzewski13@gmail.com>
2022-06-22 00:21:56 +0200

    cleaned up three class example

--- third_class.py
+++ third_class.py
@@ -1,21 +1,16 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from mne import Epochs, pick_types, concatenate_epochs
-from mne.decoding import CSP, EMS
+import pandas as pd
+import scipy.stats as st
+import seaborn as sns
+from mne import Epochs, pick_types
+from mne.decoding import CSP
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import ShuffleSplit
-from sklearn.neural_network import MLPClassifier
 
 from config import eeg_data_path
 from data_classes.subject import Subject
-import matplotlib.pyplot as plt
-
-from visualization.csp_filters import visualize_csp_filters
-import seaborn as sns
-from sklearn.metrics import accuracy_score
-import pandas as pd
-import scipy.stats as st
-
-print(__doc__)
 
 
 def create_confusion_matrix(n_classes, predictions_sets, corrects_sets):
@@ -59,22 +54,15 @@ def process(subject_name, bands, selected_channels, reg=None):
                        exclude='bads')
 
     for index, band in enumerate(bands):
-        # Read epochs (train will be done only between 1 and 2s)
-        # Testing will be done with a running classifier
         epochs.append(
             Epochs(filtered_raw_signals[index], subject.events, event_id, tmin, tmax, proj=True, picks=picks,
                    baseline=None, preload=True))
         epochs_train.append(epochs[index].copy().crop(tmin=0., tmax=2.))
 
-        # %%
-        # Classification with linear discrimant analysis
-
-        # Define a monte-carlo cross-validation generator (reduce variance):
         epochs_data.append(epochs[index].get_data())
         epochs_data_train.append(epochs_train[index].get_data())
     labels = np.array(epochs[0].events[:, -1])
-    # labels = np.where(labels == 2, 1, labels)
-    o = np.max(labels)
+
     cv = ShuffleSplit(10, test_size=0.2, random_state=42)
     cv_split = cv.split(epochs_data_train[0])
 
@@ -84,25 +72,6 @@ def process(subject_name, bands, selected_channels, reg=None):
     classifier = LinearDiscriminantAnalysis()
     csp_n_components = 10 if len(selected_channels) == 0 else min(len(selected_channels), 10)
     csp = CSP(n_components=csp_n_components, reg=reg, log=True, norm_trace=False)
-    # csp.fit_transform(epochs_data[0], labels)
-    # csp.plot_patterns(epochs[0].info, components=2, ch_type='eeg', units='Patterns (AU)', size=1.5)
-    # Use scikit-learn Pipeline with cross_val_score function
-    # clf = Pipeline([('CSP', csp), ('Classifier', classifier)])
-    # scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=None)
-
-    # Printing the results
-    # class_balance = np.mean(labels == labels[0])
-    # class_balance = max(class_balance, 1. - class_balance)
-    # print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores),
-    #                                                           class_balance))
-
-    # plot CSP patterns estimated on full data for visualization
-    # csp.fit_transform(epochs_data, labels)
-
-    # csp.plot_patterns(epochs.info, ch_type='eeg', units='Patterns (AU)', size=1.5)
-
-    # %%
-    # Look at performance over time
 
     sfreq = raw_signals[0].info['sfreq']
     w_length = int(sfreq)  # running classifier: window length
@@ -110,8 +79,6 @@ def process(subject_name, bands, selected_channels, reg=None):
     w_start = np.arange(0, epochs_data[0].shape[2] - w_length, w_step)
 
     scores_windows = []
-    correct = {0: 0, 1: 0, 2: 0}
-    all = {0: 0, 1: 0, 2: 0}
     all_predictions = []
     all_correct = []
     for train_idx, test_idx in cv_split:
@@ -131,15 +98,8 @@ def process(subject_name, bands, selected_channels, reg=None):
         classifier.fit(x_train_csp, y_train)
         X_test_csp = csp.transform(epochs_data[0][test_idx])
         predictions = classifier.predict(X_test_csp)
-        print(predictions, y_test)
         all_predictions.append(predictions)
         all_correct.append(y_test)
-
-        for i in range(len(predictions)):
-            pred = predictions[i]
-            corr = y_test[i]
-            if pred == corr: correct[corr] += 1
-            all[corr] += 1
 
         # running classifier: test classifier on sliding window
         score_this_window = []
@@ -152,144 +112,66 @@ def process(subject_name, bands, selected_channels, reg=None):
                         axis=1)
                 else:
                     x_test_csp = csp.transform(edt[test_idx][:, :, n:(n + w_length)])
-            # print(classifier.predict(x_test_csp))
-            # oko += 1
-            # print(y_test)
             score_this_window.append(classifier.score(x_test_csp, y_test))
         scores_windows.append(score_this_window)
 
-    # Plot scores over time
     w_times = (w_start + w_length / 2.) / sfreq + epochs[0].tmin
-    for i in range(3):
-        print('Predictions:', i, correct[i], all[i])
-    # plt.figure()
-    # plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
-    # plt.axvline(0, linestyle='--', color='k', label='Onset')
-    # plt.axhline(0.5, linestyle='-', color='k', label='Chance')
-    # plt.xlabel('time (s)')
-    # plt.ylabel('classification accuracy')
-    # plt.title('Classification score over time')
-    # plt.legend(loc='lower right')
-    # plt.show()
-
     return w_times, scores_windows, csp, epochs[0].info, all_predictions, all_correct
 
 
-chosen_bands = [(10., 12.)]
+def main(subjects_id):
+    chosen_bands = [(9., 14.)]
+    channels = ['C5', 'C3', 'C1', 'C2', 'C4', 'C6', 'FC3', 'CP3', 'FC4', 'CP4']
 
-chosen_bands2 = [(20., 22.)]
-channels = ['C5', 'C3', 'C1', 'C2', 'C4', 'C6', 'FC3', 'CP3', 'FC4', 'CP4']
-confusion_matrices = []
+    confusion_matrices = []
+    all_scores = {'patient': [], 'accuracy': []}
+    chances = {'values': [], 'patients': []}
+    for patient_index in subjects_id:
+        print('Processing', patient_index, 'of', len(subjects_id), 'subjects')
+        if patient_index < 10:
+            patient_name = 's0{}'.format(patient_index)
+        else:
+            patient_name = 's{}'.format(patient_index)
+        window_times, window_scores, csp_filters, epochs_info, predictions, corrects = process(patient_name,
+                                                                                               chosen_bands, channels)
+        confusion_matrices.append(create_confusion_matrix(3, predictions, corrects))
 
-# , gridspec_kw=dict(width_ratios=[4, 1, 0.2]))
+        for i in range(len(predictions)):
+            all_scores['patient'].append(patient_index)
+            all_scores['accuracy'].append(accuracy_score(predictions[i], corrects[i]))
+        chances['patients'].append(patient_index)
+        chances['values'].append(st.binom.ppf(.9, len(predictions[0]), .3) / len(predictions[0]))
 
-all_scores = {'patient': [], 'accuracy': []}
-chances = {'values': [], 'patients': []}
-for patient_index in range(1, 3):
-    print(patient_index)
-    if patient_index < 10:
-        patient_name = 's0{}'.format(patient_index)
-    else:
-        patient_name = 's{}'.format(patient_index)
-    a1, a2, a3, a4, a5, a6 = process(patient_name, chosen_bands, channels)
-    confusion_matrices.append(create_confusion_matrix(3, a5, a6))
-    this_scores = []
-    for i in range(len(a5)):
-        all_scores['patient'].append(patient_index)
-        oko = accuracy_score(a5[i], a6[i])
-        print(oko)
-        all_scores['accuracy'].append(accuracy_score(a5[i], a6[i]))
-    chances['patients'].append(patient_index)
-    chances['values'].append(st.binom.ppf(.9, len(a5[0]), .3) / len(a5[0]))
+    all_scores = pd.DataFrame(data=all_scores)
+    chances = pd.DataFrame(chances['values'], index=chances['patients']).transpose()
 
-all_scores = pd.DataFrame(data=all_scores)
-chances = pd.DataFrame(chances['values'], index=chances['patients']).transpose()
+    _accuracy_fig, accuracy_ax = plt.subplots()
+    accuracy_plot = sns.barplot(x="patient", y="accuracy", data=all_scores, ci="sd", ax=accuracy_ax, zorder=0)
+    sns.pointplot(data=chances, join=False, color='orange', ax=accuracy_ax, zorder=1)
+    accuracy_plot.set(ylim=(0, 1))
+    plt.show()
 
-# fig, ax = plt.subplots()
-# accuracy_plot = sns.barplot(x="patient", y="accuracy", data=all_scores, ci="sd",  ax=ax, zorder=0)
-# sns.pointplot(data=chances, join=False, color='orange', ax=ax, zorder=1)
-# accuracy_plot.set(ylim=(0, 1))
-# plt.show()
+    confusion_matrix_fig, confusion_matrix_ax = plt.subplots(nrows=6, ncols=9)
+    row = 0
+    column = 0
+    confusion_matrix_fig.text(0.5, 0.96, 'Predicted Class', ha='center', va='center', size='xx-large')
+    confusion_matrix_fig.text(0.06, 0.5, 'Expected Class', ha='center', va='center', size='xx-large',
+                              rotation='vertical')
+    for confusion_matrix in confusion_matrices:
+        labels = []
+        for index, matrix_row in enumerate(confusion_matrix):
+            row_sum = sum(matrix_row)
+            labels.append([])
+            for value in matrix_row:
+                labels[index].append('{}\n{:.1f}%'.format(value, value / row_sum * 100))
+        sns.heatmap(confusion_matrix, annot=labels, cbar=False, ax=confusion_matrix_ax[row, column], fmt='',
+                    cmap='inferno')
+        column += 1
+        if column > 8:
+            row += 1
+            column = 0
 
-fig, axs = plt.subplots(nrows=6, ncols=9)
-vmin = np.min(confusion_matrices)
-vmax = np.max(confusion_matrices)
-row = 0
-column = 0
-fig.text(0.5, 0.96, 'Predicted Class', ha='center', va='center', size='xx-large')
-fig.text(0.06, 0.5, 'Expected Class', ha='center', va='center', size='xx-large', rotation='vertical')
-for confusion_matrix in confusion_matrices:
-    labels = []
-    for index, matrix_row in enumerate(confusion_matrix):
-        row_sum = sum(matrix_row)
-        labels.append([])
-        for value in matrix_row:
-            labels[index].append('{}\n{:.1f}%'.format(value, value / row_sum * 100))
-    sns.heatmap(confusion_matrix, annot=labels, cbar=False, ax=axs[row, column], vmin=vmin, fmt='', cmap='inferno')
-    column += 1
-    if column > 8:
-        row += 1
-        column = 0
-
-# fig.colorbar(axs[1].collections[0], cax=axs[2])
-
-plt.show()
-# sns.heatmap(a5, annot=True,  linewidths=.5)
-# plt.show()
-# visualize_csp_filters([(10,12)], [[1,1,a3, a4]], 2)
-# plt.figure()
-# plt.plot(a1, np.mean(a2, 0), label='Score')
-# plt.axvline(0, linestyle='--', color='k', label='Onset')
-# plt.axhline(0.5, linestyle='-', color='k', label='Chance')
-# plt.xlabel('time (s)')
-# plt.ylabel('classification accuracy')
-# plt.title('Classification score over time')
-# plt.legend(loc='lower right')
-# plt.show()
+    plt.show()
 
 
-# # b1, b2, _b3, _b4 = process('s14', chosen_bands2, channels)
-# fig, (ax1, ax2) = plt.subplots(1)
-# fig.suptitle('Vertically stacked subplots')
-# ax1.plot(a1, np.mean(a2, 0))
-# # ax2.plot(b1, np.mean(b2, 0))
-# plt.axvline(0, linestyle='--', color='k', label='Onset')
-# plt.axhline(0.5, linestyle='-', color='k', label='Chance')
-# plt.xlabel('time (s)')
-# plt.ylabel('classification accuracy')
-# plt.title('Classification score over time')
-# plt.legend(loc='lower right')
-# plt.show()
-# plt.figure()
-# plt.subplot(211,a1, np.mean(a2, 0), label='Score1')
-# plt.subplot(212,b1, np.mean(b2, 0), label='Score2')
-# plt.axvline(0, linestyle='--', color='k', label='Onset')
-# plt.axhline(0.5, linestyle='-', color='k', label='Chance')
-# plt.xlabel('time (s)')
-# plt.ylabel('classification accuracy')
-# plt.title('Classification score over time')
-# plt.legend(loc='lower right')
-# plt.show()
-
-
-# values = {}
-# v = []
-# # for i in range(2):
-# #     if i < 9:
-# #         subject = 's0{}'.format(i + 1)
-# #         values[subject] = np.mean(process(subject))
-# #         v.append(np.mean(process(subject)))
-# #     else:
-# #         subject = 's{}'.format(i + 1)
-# #         values[subject] = np.mean(process(subject))
-# #         v.append(np.mean(process(subject)))
-#
-# for i in range(17):
-#     f_min = i * 2 + 4
-#     f_max = i * 2 + 6
-#     name = '{}Hz-{}Hz'.format(f_min, f_max)
-#     values[name] = np.mean(process('s14', f_min, f_max))
-#     v.append(values[name])
-#
-# plt.bar(np.arange(1, 18), v)
-# plt.show()
+main(range(1, 53))
